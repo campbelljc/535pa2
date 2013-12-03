504149cee0d2c5303f2ff7e001a0c21971168058
jothesmo <jothesmo@gmail.com>
2013-12-03 13:12:07 -0500

    blah

--- src/grouter/grouter.c
+++ src/grouter/grouter.c
@@ -79,8 +79,8 @@ int main(int ac, char *av[])
 
 	// add a default Queue.. the createClassifier has already added a rule with "default" tag
 	// char *qname, char *dqisc, double qweight, double delay_us, int nslots);
-	addPktCoreQueue(pcore, "default", "taildrop", 1.0, 2.0, 0);
-	addPktCoreQueue(pcore, "attack", "taildrop", 5.0, 2.0, 0);
+	addPktCoreQueue(pcore, "default", "taildrop", 10.0, 2.0, 0);
+	addPktCoreQueue(pcore, "attack", "taildrop", 0.0, 2.0, 0);
 	rconfig.scheduler = PktCoreSchedulerInit(pcore);
 	rconfig.worker = PktCoreWorkerInit(pcore);
 
